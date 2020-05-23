a2dd9aecf5558c34cbcbb7308a1e754c89cec1bc
arvind ram <arvindram03@gmail.com>
2020-05-23 02:52:45 -0700

    Adding quick start and cli commands

--- sidebars.js
+++ sidebars.js
@@ -2,11 +2,6 @@ module.exports = {
   someSidebar: {
     Truss: ['intro','concepts','example'],
     GettingStarted: ['quick_start', 'how_to'],
-    CLI: ['intro'],
-    Projects: ['intro'],
-    Domains: ['intro'],
-    Gateways: ['intro'],
-    Deploy: ['infra'],
-    Clients: ['infra'],
+    CLI: ['commands'],
   },
 };
