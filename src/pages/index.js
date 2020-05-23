d0a1fdb8bdd6b2e944271fa6dd7fce165aa873ee
arvind ram <arvindram03@gmail.com>
2020-05-23 03:26:53 -0700

    Fix title

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,8 +61,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Particle, a better way to build backends<head />">
+      title={`${siteConfig.title}`}
+      description="Truss, a better way to build backends<head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
