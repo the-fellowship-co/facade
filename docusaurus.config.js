db86bd3e4d991fa4c5edb5bb53522e1048bdd9d1
arvind ram <arvindram03@gmail.com>
2020-05-23 03:15:51 -0700

    Setting edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/alphas-co/facade/edit/master/',
         },
         blog: {
           showReadingTime: true,
