075c515ccb6204017f5cfcc9cfdc0b487d85fcc3
Sunil <sunilvigneshr@gmail.com>
2020-06-22 01:50:39 -0700

    baseurl changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Better way to build backends',
   tagline: 'Design, develop and deploy backends incredibly faster.',
   url: 'https://letsbyld.com',
+  baseUrl: '/',
   favicon: 'favicon/ico',
   organizationName: 'the-fellowship-co', // Usually your GitHub org/user name.
   projectName: 'facade', // Usually your repo name.
