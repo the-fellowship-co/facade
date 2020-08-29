31b2575e30b102bf63679b127cc89fb73f86351d
Sunil <sunilvigneshr@gmail.com>
2020-08-29 12:46:30 -0700

    remove css baseline

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import CodeBlock from '@theme/CodeBlock';
 import Link from '@docusaurus/Link';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
@@ -273,7 +272,6 @@ function Home() {
     <Layout
       title="Byld"
       description="Byld, a better way to build backends<head/>">
-      <CssBaseline />
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
         <div className="row">
